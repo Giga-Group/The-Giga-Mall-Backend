2064b0c12d52c00fe7e379d588b7599e8ec287ff
alwahabkhan <alwahabalikhan8@gmail.com>
2026-02-23 15:04:04 +0500

    changes for supabase

--- src/database/run-migrations.ts
+++ src/database/run-migrations.ts
@@ -6,6 +6,7 @@ dotenv.config();
 const dataSource = new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL, // Complete connection string: postgresql://username:password@host:port/database
+  ssl: process.env.DATABASE_URL?.includes('supabase') ? { rejectUnauthorized: false } : false,
   entities: [__dirname + '/../entities/*.entity.{ts,js}'],
   migrations: [__dirname + '/migrations/*.{ts,js}'],
   synchronize: false,
